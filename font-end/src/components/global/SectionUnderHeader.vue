4f7d790ae699f347a61df3889178bcaea0de159a
abdeljail <taki22abdo@gmail.com>
2022-04-19 06:04:27 +0200

    I add a folder Contact in content component  pack informations :file_folder:  a: :100: :label:

---
+++ font-end/src/components/global/SectionUnderHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <h1 class="m-0 p-3 text-center">{{ heading }}</h1>
+    <p class="m-0 p-4 text-center">{{ paragraph }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "SectionUnderHeader",
+  data() {
+    return {};
+  },
+  props: {
+    heading: {
+      type: String,
+      require: true,
+    },
+    paragraph: {
+      type: String,
+      require: true,
+      default: "",
+    },
+  },
+  created(){
+    console.log(this.paragraph);
+  }
+};
+</script>
+<style scoped>
+div {
+  height: 85vh;
+  background: url("../../assets/images/sectionunderheader.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 120%;
+}
+h1{
+    color: var(--second);
+}
+p {
+  font-size: 18px;
+  min-width: 280px;
+  max-width: 500px;
+  color: var(--cl-paragraph);
+}
+</style>
