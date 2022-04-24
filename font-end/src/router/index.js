e6909c1663a24ac853cbd4aab0ff35a259e59fbb
abdeljail <taki22abdo@gmail.com>
2022-04-25 00:14:09 +0200

    completed coding in parte design web in client

--- font-end/src/router/index.js
+++ font-end/src/router/index.js
@@ -6,55 +6,109 @@ import Skills from "../views/Skills.vue"
 import TrainingOffer from "../views/TrainingOffer.vue"
 import SinIn from "../views/SingIn.vue"
 import SingUp from "../views/SingUp.vue"
+import WhoAreWe from "../views/WhoAreWe.vue"
+import ApplicationRI from "../views/ApplicationRI.vue"
+import RespanseApplication from "../components/ApplicationRI/RespanseApplication.vue";
+import SelectTypeApplication from "../components/ApplicationRI/SelectTypeApplication.vue"
+import UploadCv from "../components/ApplicationRI/UploadCv.vue"
 
 Vue.use(VueRouter);
 
 const routes = [{
-    path: "/",
-    name: "HomeView",
-    component: HomeView,
+  path: "/",
+  alias: '/home',
+  name: "HomeView",
+  component: HomeView,
+},
+{
+  path: "/about",
+  name: "about",
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: function () {
+    return import( /* webpackChunkName: "about" */ "../views/AboutView.vue");
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import( /* webpackChunkName: "about" */ "../views/AboutView.vue");
+},
+{
+  path: "/contact-us",
+  name: "ContactUs",
+  component: ContactUs,
+},
+{
+  path: "/skills",
+  name: "Skills",
+  component: Skills,
+},
+{
+  path: "/training-offer",
+  name: "TrainingOffer",
+  component: TrainingOffer
+},
+{
+  path: "/who-are-we",
+  name: "Who-are-we",
+  component: WhoAreWe
+},
+{
+  path: "/application-r-i/",
+  name: "ApplicationRI",
+  component: ApplicationRI,
+  children: [{
+    path: '/',
+    name: "SelectTypeApplication",
+    component: SelectTypeApplication,
+    meta: {
+      header: 0
     },
   },
   {
-    path: "/contact-us",
-    name: "ContactUs",
-    component: ContactUs,
-  },
-  {
-    path: "/skills",
-    name: "Skills",
-    component: Skills,
-  },
-  {
-    path: "/training-offer",
-    name: "TrainingOffer",
-    component: TrainingOffer
-  },
-  {
-    path: "/sing-in",
-    name: "SinIn",
-    component: SinIn,
+    path: "employment",
+    name: "Application",
+    component: UploadCv,
+    props: route => ({
+      name: route.path
+    }),
     meta: {
       header: 0
-    }
+    },
+
   },
   {
-    path: "/sing-up",
-    name: SingUp,
-    component: SingUp,
+    path: "internship",
+    name: "Application",
+    component: UploadCv,
     meta: {
       header: 0
-    }
+    },
+
   }
+  ],
+},
+{
+  path: "/respanse-application/",
+  name: "RespanseApplication",
+  component: RespanseApplication,
+  meta: {
+    header: 0
+  },
+},
+{
+  path: "/sing-in",
+  name: "SinIn",
+  component: SinIn,
+  meta: {
+    header: 0
+  }
+},
+{
+  path: "/sing-up",
+  name: SingUp,
+  component: SingUp,
+  meta: {
+    header: 0
+  }
+}
 ];
 
 const router = new VueRouter({
