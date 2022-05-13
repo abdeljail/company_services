4ef19ce2cb8e29a2666155168673dc2f8561c726
abdeljail <taki22abdo@gmail.com>
2022-05-14 01:32:29 +0200

    I complited in section in contains heading h2 and link for front end :tada: :label:

---
+++ font-end/src/components/admin/global/H2Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-container>
+    <div class="py-4 d-flex justify-content-between">
+      <div>
+        <h2 class="text-capitalize">{{ heading }}</h2>
+      </div>
+      <div v-if="objLink" instanceof Object>
+        <router-link
+          class="btn link text-capitalize text-white"
+          :to="objLink.pathLink"
+          >{{ objLink.nameLink }}</router-link
+        >
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "H2Button",
+  data() {
+    return {};
+  },
+  props: {
+    heading: {
+      type: String,
+      require: true,
+    },
+    objLink: {
+      nameLink: {
+        type: String,
+        require: true,
+      },
+      pathLink: {
+        type: String,
+        require: true,
+      },
+      default: null,
+    },
+  },
+};
+</script>
+<style>
+.link {
+  background-color: var(--second) !important;
+}
+</style>
