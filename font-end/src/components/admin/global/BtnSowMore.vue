913bc23307b737bb45afc68dcb7ffd4f258d2913
abdeljail <taki22abdo@gmail.com>
2022-05-15 06:08:29 +0200

    I complited section button in show more data for front end :tada: :label:

---
+++ font-end/src/components/admin/global/BtnSowMore.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="
+      py-3
+      d-flex
+      align-items-center
+      justify-content-center
+      show-more-category
+      bg-light
+    "
+  >
+    <button
+      data-loading="show more"
+      :disabled="$attrs['loading']"
+      :class="$attrs['loading'] ? 'loading' : ''"
+      @click="$emit('show', $attrs['data-number-show'])"
+      class="
+        d-flex
+        align-items-center
+        justify-content-center
+        text-capitalize text-white
+        border-0
+        btn
+      "
+    ></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  created() {},
+};
+</script>
+
+<style scoped>
+button {
+  box-shadow: none !important;
+}
+button {
+  background-color: var(--primary);
+  width: 150px;
+}
+button::after {
+  content: attr(data-loading);
+}
+button:disabled:after,
+button.loading:after {
+  display: block;
+  content: "";
+  border: 6px solid #eee;
+  animation: loding 1s linear infinite;
+  border-bottom-color: var(--second);
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+}
+@keyframes loding {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
