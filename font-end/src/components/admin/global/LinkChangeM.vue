ae0cd1b1c802718e72a5b2a1ac8eaf563c9f2a90
abdeljail <taki22abdo@gmail.com>
2022-05-16 15:50:27 +0200

    I complited section change pages :tada: :label:

---
+++ font-end/src/components/admin/global/LinkChangeM.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex pb-5 links justify-content-end">
+    <router-link
+      :to="$attrs['link']"
+      class="px-4 py-2 text-white text-decoration-none rounded text-capitalize"
+      >{{ $attrs["name"] }}
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "LinkChangeM",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.links a {
+  background-color: var(--primary);
+  margin-top: -20px;
+}
+</style>
