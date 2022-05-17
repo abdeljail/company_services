d50239d3454c2096af3a358ef1f9ca1b356a7c3e
abdeljail <taki22abdo@gmail.com>
2022-05-17 19:00:22 +0200

    I complited add category :tada: :label:

---
+++ backEnd/routers/addCat.router.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const ControllerAddNameCat = require("../controllers/addCat.controller");
+
+router.post("/add-categoty", ControllerAddNameCat.addNameCat);
+
+module.exports = router;
