0335c039b3869b351a34d32643643d5bf6407e68
abdeljail <taki22abdo@gmail.com>
2022-05-17 03:53:09 +0200

    I update in functions  checkNameCat and submitData :zap:  :label:

--- font-end/src/admin/AddedCategory.vue
+++ font-end/src/admin/AddedCategory.vue
@@ -1,97 +1,96 @@
 <template>
   <div class="AddedTraining">
-    <SectionUnder heading="Added Training" />
-    <div class="form-data">
-      <div
-        class="
-          content
-          d-flex
-          align-items-center
-          justify-content-center
-          flex-column
-        "
-      >
-        <form class="form p-4 bg-light" @submit.prevent="submitData()">
-          <div class="header-from">
-            <h2>Added Training</h2>
-          </div>
-          <div class="body-from">
-            <div class="input-name py-2">
-              <label for="name" class="text-capitalize py-2 d-block"
-                >name categopry</label
-              >
-              <input
-                class="border-1 p-2 w-100 rounded"
-                type="text"
-                name="name"
-                id="name"
-                v-model="nameCat"
-                @blur="checkNameCat()"
-              />
-            </div>
-            <div class="btn-submit py-2 d-flex justify-content-center">
-              <button
-                class="
-                  border
-                  d-flex
-                  justify-content-center
-                  align-itens-center
-                  d-block
-                  text-white
-                  px-5
-                  rounded
-                  py-2
-                  text-capitalize
-                "
-                type="submit"
-                :disabled="check"
-                :class="check ? 'send-data' : ''"
-              ></button>
-            </div>
-          </div>
-        </form>
+    <SectionUnder heading="Added Category" />
+    <b-container>
+      <LinkChangeM link="/management-categories/"  name="management categories"/>
+      <div class="form-data">
         <div
-          v-if="Boolean(Object.keys(this.objAlert).length)"
-          class="res-alert position-relative"
+          class="
+            content
+            d-flex
+            align-items-center
+            justify-content-center
+            flex-column
+          "
         >
-          <Alert @emptyObjAlert="emptyObjAlert" :objAlert="objAlert" />
+          <form class="form p-4 bg-light" @submit.prevent="submitData()">
+            <div class="header-from">
+              <h2>Added Category</h2>
+            </div>
+            <div class="body-from">
+              <div class="input-name py-2">
+                <label for="name" class="text-capitalize py-2 d-block"
+                  >name categopry</label
+                >
+                <input
+                  class="border-1 p-2 w-100 rounded"
+                  type="text"
+                  name="name"
+                  id="name"
+                  v-model="nameCat"
+                  @blur="checkNameCat()"
+                />
+              </div>
+              <div class="btn-submit py-2 d-flex justify-content-center">
+                <button
+                  class="
+                    border
+                    d-flex
+                    justify-content-center
+                    align-itens-center
+                    d-block
+                    text-white
+                    px-5
+                    rounded
+                    py-2
+                    text-capitalize
+                  "
+                  type="submit"
+                  :disabled="check"
+                  :class="check ? 'send-data' : ''"
+                ></button>
+              </div>
+            </div>
+          </form>
+          <div
+            v-if="Boolean(Object.keys(this.objAlert).length)"
+            class="res-alert position-relative"
+          >
+            <Alert @emptyObjAlert="emptyObjAlert" :objAlert="objAlert" />
+          </div>
         </div>
       </div>
-    </div>
+    </b-container>
   </div>
 </template>
 <script>
 import axios from "axios";
-import Alert from "@/components/admin/global/alert.vue";
 import SectionUnder from "@/components/admin/global/SectionUnder.vue";
+import LinkChangeM from "@/components/admin/global/LinkChangeM.vue";
+import Alert from "@/components/admin/global/alert.vue";
 export default {
   name: "AddedCategory",
   data() {
     return {
       nameCat: "",
-      nameCatInput: "",
       check: false,
-      objAlert: {
-        // nameClass: "",
-        // message: "",
-      },
+      objAlert: {},
     };
   },
   components: {
     SectionUnder,
+    LinkChangeM,
     Alert,
   },
   methods: {
     checkNameCat() {
       if (this.nameCat === "" || this.nameCat.length <= 4) {
-        this.nameCatInput = false;
-        return;
+        return false;
       }
-      this.nameCatInput = true;
+      return true;
     },
     submitData() {
-      this.checkNameCat();
-      if (!this.nameCatInput) return;
+      if (!this.checkNameCat()) return;
       if (Boolean(Object.keys(this.objAlert).length)) this.emptyObjAlert();
       this.check = !this.check;
       axios
@@ -100,17 +99,13 @@ export default {
         })
         .then((response) => {
           this.check = !this.check;
-          console.log(this.check);
+          this.nameCat = "";
           if (response.data.message) {
             this.objAlert.nameClass = "alert-success";
-            this.objAlert.message = "Item added successfully";
-            return;
+            return (this.objAlert.message = "Item added successfully");
           }
-          if (!response.data.message) {
-            this.objAlert.nameClass = "alert-warning";
-            this.objAlert.message = "This item exists";
-            return;
-          }
+          this.objAlert.nameClass = "alert-warning";
+          this.objAlert.message = "This item exists";
         })
         .catch((error) => {
           this.check = !this.check;
@@ -118,7 +113,6 @@ export default {
         });
     },
     emptyObjAlert() {
-      console.log("dldldldldl");
       this.objAlert = {};
     },
   },
