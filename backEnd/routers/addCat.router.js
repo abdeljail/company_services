ed0dd4296e8b69fdc1dab9df71c725affc157f59
abdeljail <taki22abdo@gmail.com>
2022-05-19 17:02:35 +0200

    I fixed  :tada: :labe:

--- backEnd/routers/addCat.router.js
+++ backEnd/routers/addCat.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const ControllerAddNameCat = require("../controllers/addCat.controller");
 
+
+
 router.post("/add-categoty", ControllerAddNameCat.addNameCat);
 
 module.exports = router;
