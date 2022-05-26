7d8cb3ff8815119ccd8ce421466f1af2eec245c5
abdeljail <taki22abdo@gmail.com>
2022-05-26 21:36:40 +0200

    ..

--- font-end/vue.config.js
+++ font-end/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   lintOnSave: false,
+  devServer: {
+    host: process.env.HOST || 'localhost',
+    open: true,
+    port: process.env.PORT || 8082,
+    proxy:process.env.VUE_APP_URL,
+  },
 };
