50d01b579b3364cd9444a7c5d43626388caf6938
abdeljail <taki22abdo@gmail.com>
2022-05-28 22:23:12 +0200

    footer and header in pafes  :100: :tada: :label:

---
+++ font-end/src/components/sing-in-up/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="mt-5 border-top">
+    <b-container>
+      <Language />
+    </b-container>
+  </footer>
+</template>
+
+<script>
+import Language from "@/components/global/Language.vue";
+export default {
+  name: "Footer",
+  data() {
+    return {};
+  },
+  components: {
+    Language,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ font-end/src/components/sing-in-up/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="pb-3">
+    <div class="py-3 text-center">
+      <router-link
+        class="text-decoration-none text-white px-3 py-2 rounded back-home"
+        to="/"
+      >
+        <img class="logo-image" :src="logoImage" alt="image logo" />
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+import Header from "../admin/global/Header.vue";
+export default {
+  components: { Header },
+  name: "Header",
+  data() {
+    return {};
+  },
+  inject: ["logoImage"],
+};
+</script>
+
+<style>
+</style>
