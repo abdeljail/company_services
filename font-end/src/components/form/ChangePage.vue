45a643829198163d750967f3e9da33d7e4de52a6
abdeljail <taki22abdo@gmail.com>
2022-05-28 22:19:39 +0200

    I add cpmponent for change pages sing-in and sing-up for link-router :100: :tada: :label:

---
+++ font-end/src/components/form/ChangePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-3 border-top mt-4">
+    <router-link
+      id="a-link"
+      class="a-link text-decoration-none text-capitalize d-block rounded a-link"
+      :to="path"
+    >
+      {{ name }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChangePage",
+  data() {
+    return {};
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
