f24acd58718c6ae201131276b1b96859c8b1308c
abdeljail <taki22abdo@gmail.com>
2022-05-28 22:14:33 +0200

    I changed global style in css for folder css in aseets :100: :tada: :label:

--- font-end/src/App.vue
+++ font-end/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="position-relative" id="app">
     <Header v-if="$route.meta.header === 1" />
     <HeaderAdmin
       :updateShowAside="updateShowAside"
@@ -64,51 +64,4 @@ export default {
 };
 </script>
 <style>
-@import url("http://fonts.cdnfonts.com/css/cubano");
-:root {
-  --primary: #238a91;
-  --second: #0e314c;
-  --bc-white: #ffffff;
-  --bc-hover-link: #238a9133;
-  --cl-paragraph: #454e56;
-  --font-link: "Cubano", sans-serif;
-  --font-paragraph: Helvetica, Arial, sans-serif;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-* {
-  user-select: none;
-}
-.logo-image {
-  width: 60px;
-  height: 60px;
-}
-.content-router-aside > div:last-child {
-  height: 100vh;
-}
-.content-router-aside > div:last-child {
-  flex-grow: 1.5;
-}
-
-.content-router-aside.active .aside-show {
-  position: absolute !important;
-  left: -300px;
-}
-
-.content-router-aside.active .router-w-100 {
-  width: 100%;
-}
-.model {
-  background-color: #0e314c70;
-  z-index: -1;
-  transition: 0.2s ease-in;
-  opacity: 0;
-}
-.overly {
-  opacity: 1;
-  z-index: 10;
-}
 </style>
