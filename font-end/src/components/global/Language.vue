c890a17a28a03b38e964fa184cc67228d7e965a6
abdeljail <taki22abdo@gmail.com>
2022-05-28 22:20:44 +0200

    I add cpmponent for change language in application :100: :tada: :label:

---
+++ font-end/src/components/global/Language.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex justify-content-between align-items-center flex-wrap">
+    <div>
+      <p class="p-0 m-0">Copyright company services © 2020</p>
+    </div>
+    <div class="p-3 text-end">
+      <button
+        class="
+          color-h-white
+          px-3
+          py-2
+          color-primary
+          none-bg-primary
+          border-2 border
+          rounded
+        "
+      >
+        Language
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Language",
+};
+</script>
+
+<style>
+</style>
