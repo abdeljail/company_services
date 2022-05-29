488d5b067a46c873e6626b5f8f95ae2c1c3789a7
abdeljail <taki22abdo@gmail.com>
2022-05-29 22:11:38 +0200

    I complited dynamic component :100: :label:

---
+++ font-end/src/components/form/Text.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="">
+    <input
+      :class="[errText ? '' : 'input-err', name === 'Last' ? 'my-3' : '']"
+      class="px-2 font-14 py-3 rounded w-100 border"
+      type="text"
+      :name="name"
+      :placeholder="`${name} name `"
+      :ref="name"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "Text",
+  data() {
+    return {};
+  },
+  props: {
+    errText: {
+      type: Boolean,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  created() {},
+};
+</script>
+<style>
+</style>
