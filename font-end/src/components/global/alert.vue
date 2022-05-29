4226c346d050592ca05efe8876fc62c62830ab3b
abdeljail <taki22abdo@gmail.com>
2022-05-29 22:08:41 +0200

    I update props alert and classname , is daynamic for show message :zap: :label:

--- font-end/src/components/global/alert.vue
+++ font-end/src/components/global/alert.vue
@@ -13,21 +13,19 @@
       text-white
     "
     role="alert"
-    :class="objAlert.className ? objAlert.className : ''"
+    :class="className ? className : ''"
   >
     <b-container>
       <div class="d-flex justify-content-between">
         <div>
           <div
-            v-html="objAlert.password !== undefined ? objAlert.password : ''"
-          ></div>
-          <div
-            class
-            v-html="objAlert.email !== undefined ? objAlert.email : ''"
+            v-for="(message, idx) in Alert"
+            :key="idx"
+            v-html="message"
           ></div>
         </div>
         <div>
-          <span role="button" tabindex="0" @click="$emit('emptyObjAlert')"
+          <span role="button" tabindex="0" @click="$emit('emptyAlert')"
             ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
               <path
                 d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
@@ -45,12 +43,18 @@ export default {
     return {};
   },
   props: {
-    objAlert: {
-      type: Object,
+    Alert: {
+      type: Array,
+      require: true,
+    },
+    className: {
+      type: String,
       require: true,
     },
   },
-  created() {},
+  created() {
+    console.log(this.Alert);
+  },
 };
 </script>
 
